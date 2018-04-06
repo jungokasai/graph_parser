1352698f3b06cdf4886f7b9fd228d21b7f18add5
Jungo Kasai <jungo@michelangelo.ling.yale.edu>
2018-04-06 01:02:30 -0400

    add images

--- unbounded_dependencies/eval/dot2graph.py
+++ unbounded_dependencies/eval/dot2graph.py
@@ -11,7 +11,7 @@ def convert(dot_file):
     final_file = dot_file[:-2] + 'pdf'
     command = 'dot -Tps {} -o {}'.format(dot_file, new_file)
     subprocess.check_call(command, shell=True)
-    command = 'convert --density 500 {} {}'.format(new_file, final_file)
+    command = 'convert {} {}'.format(new_file, final_file)
     subprocess.check_call(command, shell=True)
 if __name__ == '__main__':
     constructions = ['obj_extract_rel_clause', 'obj_extract_red_rel', 'sbj_extract_rel_clause', 'obj_free_rels', 'obj_qus', 'right_node_raising', 'sbj_embedded']
